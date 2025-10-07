d6f68bf40801cfc8e8084430c67d8c586668b7ba
Jelena810-bytes <andrijevic.j@gmail.com>
2025-10-07 15:25:23 +0200

    working, changing and testing functions in the game

--- js/app.js
+++ js/app.js
@@ -1,148 +1,125 @@
 /*---------------------------- Variables (state) ----------------------------*/
 
-const state = {
-  boredom: 0,
-  hunger: 0,
-  sleepiness: 0
+let state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0
 };
 
-let timer = null;
-
+let timer;
 let gameOver = false;
 
 
 /*------------------------ Cached Element References ------------------------*/
 
 const boredomStatEl = document.getElementById('boredom-stat');
-
-console.log('boredomStatEl', boredomStatEl)
-
 const hungerStatEl = document.getElementById('hunger-stat');
-
-console.log('hungerStateEl', hungerStatEl)
-
 const sleepinessStatEl = document.getElementById('sleepiness-stat');
 
-console.log('sleepinessStatEl', sleepinessStatEl)
 
 
 const playBtnEl = document.getElementById('play');
 const feedBtnEl = document.getElementById('feed');
-const sleepBtnEl = document.getElementById('sleep')
+const sleepBtnEl = document.getElementById('sleep');
+
+
 
 
 const gameMessageEl = document.getElementById('message');
-console.log('gameMessageEl', gameMessageEl)
 
-const resetBtnEl= document.querySelector('#restart');
+const resetBtnEl = document.getElementById('restart');
+
 
 
 /*-------------------------------- Functions --------------------------------*/
 
+
+
 function init() {
+ // resetBtnEl.addEventListener("click", init);
+    resetBtnEl.classList.add("hidden");
+    gameMessageEl.classList.add("hidden");
+    state.boredom = 0;
+    state.hunger = 0;
+    state.sleepiness = 0;
+    timer = setInterval(runGame, 2000);
+    gameOver = false;
+    render();
 
-state.boredom = 0;
-state.hunger = 0;
-state.sleepiness = 0;
-gameOver = false;
-
-
-timer = setInterval(runGame, 2000);
 }
-console.log('Initializing game...')
-
-function checkGameOver() {
-
-    if (
-    state.boredom >= 10  ||
-    state.hunger >= 10  ||
-    state.sleepiness >= 10  
-    ) {
-        gameOver = true;
-    }
-}
- 
-
 
 function runGame() {
     updateStates();
     checkGameOver();
     render();
-
 }
-console.log('The game is running!')
+
 
 function render() {
 
+    boredomStatEl.textContent = state.boredom;
+    hungerStatEl.textContent = state.hunger;
+    sleepinessStatEl.textContent = state.sleepiness;
+
+    if (gameOver) {
+        gameMessageEl.textContent = "Game Over! Your pet needs too much care."
+        resetBtnEl.classList.remove('hidden');
+        gameMessageEl.classList.remove('hidden');
+        clearInterval(timer);
+
+
+    }
 }
-console.log('Rendering game state...')
-
-
-    /*----------------------------- Event Listeners -----------------------------*/
 
 function updateStates() {
+    // console.log('')
+    function getRandomIncrement() {
+        return Math.floor(Math.random() * 4);
+    }
+    state.boredom += getRandomIncrement();
+    state.hunger += getRandomIncrement();
+    state.sleepiness += getRandomIncrement();
+    //console.log('boredom', state.boredom)
 
-state.boredom +=Math.floor(Math.random() * 4);
-state.hunger += Math.floor(Math.random() * 4);
-state.sleepiness += Math.floor(Math.random() * 4);
-
-}
-
-function render() {
-boredomStatEl.textContent = state.boredom;
-hungerStatEl.textContent = state.hunger;
-sleepinessStatEl.textContent = state.sleepiness;
-  if (gameOver) {
-    clearInterval(timer);
-    gameMessageEl.textContent = "Game Over! Your pet needs too much care."
-  }
 
 }
-console.log()
+function checkGameOver() {
+
+    if (
+        state.boredom >= 10 ||
+        state.hunger >= 10 ||
+        state.sleepiness >= 10
+    ) {
+        gameOver = true;
+    }
+}
+
 
 function playBtnClick() {
-    console.log("clicked")
     state.boredom = 0;
-    render()
+    render();
 
-
+}
 playBtnEl.addEventListener('click', playBtnClick);
-}
 
-function hunger() {
+
+function feedBtnClick() {
     state.hunger = 0;
-    render()
+    render();
 
-
-feedBtnEl.addEventListener('click', hunger);
 }
+feedBtnEl.addEventListener('click', feedBtnClick);
 
-function sleepiness() {
+
+function sleepBtnClick() {
     state.sleepiness = 0;
-    render()
-
-
-sleepBtnEl.addEventListener('click', sleepiness);
+    render();
 }
+sleepBtnEl.addEventListener('click', sleepBtnClick);
 
 
 
-function init() {
-   resetBtnEl.classList.add('hidden');
-   gameMessageEl.classList.add('hidden');
-
-
-   state.hunger = 0;
-   state.boredom = 0;
-   state.sleepiness = 0;
-
-   render()
-}
-
-resetBtnEl.addEventListener('click', init);
-
-
-
+init()
 
 
 
